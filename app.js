7ba4efc5c8e24528bc2b0268b7844e243874b1ed
Pablo Reszczynski <pablore@me.com>
2017-10-20 15:33:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const restify = require('restify');
+const builder = require('botbuilder');
+const axios = require('axios');
+
+const cAPI = 'https://api.cryptomkt.com/v1'
+
+const server = restify.createServer();
+server.listen(process.env.PORT || 3978, () => {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+const bot = new builder.UniversalBot(connector, session => {
+  const ticker = axios.get(`${cAPI}/ticker?market=ETHCLP`).then(res => {
+    const data = res.data.data[0];
+    console.log(data);
+    session.send('El precio de ETH esta en %s CLP', data.last_price);
+  });
+});
